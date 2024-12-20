ee3467d4500b35bb9b1dd4180f05deb5c602edaa
Connor Payne <connor@stakehauslabs.com>
2024-12-20 14:21:43 +0100

    add react example

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { NftVerifier } from './sdk/NftVerifier';
 export { WalletConnector } from './sdk/WalletConnector';
 export { OwnershipCheckRequest } from './sdk/types';
+export { WalletWidget } from './components/WalletWidget';
